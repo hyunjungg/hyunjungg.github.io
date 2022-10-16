361b6eb248817e41d05c5339194fbdc6ffa54e09
hyunjungg <hj351016@gmail.com>
2022-10-17 01:25:30 +0900

    change url

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -3,10 +3,10 @@ module.exports = {
   description: `보안과 개발을 좋아하는 학생입니다~^^`,
   author: `Hyunjung`,
   introduction: `보안과 개발을 좋아하는 학생입니다~^^`,
-  siteUrl: `https://hyunjunggg.github.io`, // Your blog site url
+  siteUrl: `https://hyunjungg.github.io`, // Your blog site url
   social: {
     twitter: `windows_cool`, // Your Twitter account
-    github: `hyunjunggg`, // Your GitHub account
+    github: `hyunjungg`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
     linkedin: ``, // Your LinkedIn account
@@ -16,7 +16,7 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'hyunjunggg/hyunjunggg.github.io', // Your repository for archive comment
+    utterances: 'hyunjungg/hyunjungg.github.io', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -4,7 +4,7 @@ import './index.scss'
 
 export const Footer = () => (
   <footer className="footer">
-    ©<a href="https://github.com/hyunjunggg">Hyunjung</a>, Built with{' '}
+    ©<a href="https://github.com/hyunjungg">Hyunjung</a>, Built with{' '}
     <a href="https://github.com/JaeYeopHan/gatsby-starter-bee">
       Gatsby-starter-bee
     </a>
